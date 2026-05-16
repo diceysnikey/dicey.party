bcbfe9b20cb3d48325ffadb579ec6772809dd0b3
diceysnikey <diceysnikey@gmail.com>
2026-05-16 18:23:17 +0200

    Implemented support for post-quantum key exchange so the terminal shuts up

--- go.mod
+++ go.mod
@@ -30,9 +30,9 @@ require (
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/crypto v0.51.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
-	golang.org/x/sync v0.12.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/sys v0.44.0 // indirect
+	golang.org/x/text v0.37.0 // indirect
 )
--- test.go
+++ test.go
@@ -1,1 +1,58 @@
 package main
+
+import (
+	"errors"
+	"net"
+
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	gossh "golang.org/x/crypto/ssh"
+)
+
+const (
+	host = "localhost"
+	port = "2222"
+)
+
+func main() {
+	srv, err := wish.NewServer(
+		// The address the server will listen to.
+		wish.WithAddress(net.JoinHostPort(host, port)),
+
+		// The SSH server need its own keys, this will create a keypair in the
+		// given path if it doesn't exist yet.
+		// By default, it will create an ED25519 key.
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		// Middlewares do something on a ssh.Session, and then call the next
+		// middleware in the stack.
+		wish.WithMiddleware(
+			func(next ssh.Handler) ssh.Handler {
+				return func(sess ssh.Session) {
+					wish.Println(sess, "Hello, world!")
+					next(sess)
+				}
+			},
+		),
+	)
+	srv.ServerConfigCallback = func(ctx ssh.Context) *gossh.ServerConfig {
+		return &gossh.ServerConfig{
+			Config: gossh.Config{
+				KeyExchanges: []string{
+					"mlkem768x25519-sha256",
+					"sntrup761x25519-sha512",
+				},
+			},
+		}
+	}
+
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	log.Info("Starting SSH server", "host", host, "port", port)
+	if err = srv.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		// We ignore ErrServerClosed because it is expected.
+		log.Error("Could not start server", "error", err)
+	}
+}
